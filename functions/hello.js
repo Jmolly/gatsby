098deeb283a933da3e1f5beb9429788f9b5d4a80
jmolly <roshkovskypetr@gmail.com>
2021-01-28 01:05:13 +0300

    three

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,64 @@
-exports.handler = async function(event, context) {
-  console.log('hey');
+import { google } from 'googleapis'
+import { config as dotenvConfig } from 'dotenv'
 
-  return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Hello World"})
-  };
+dotenvConfig()
+
+const getClient = ({ scopes }) => {
+  console.log(scopes, 'getclietn');
+
+  return google.auth.getClient({
+    credentials: JSON.parse(
+      Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT, 'base64').toString(
+        'ascii'
+      )
+    ),
+    scopes: scopes,
+  })
+}
+
+const authorizeSheets = async () => {
+  const client = await getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  })
+  return google.sheets({
+    version: 'v4',
+    auth: client,
+  })
+}
+
+const addToCol = async (range, emailAddress) => {
+  const sheets = await authorizeSheets()
+  return new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.append(
+      {
+        spreadsheetId: '1M73-c45jziO-QQgLNOTC5JL-FseZFhSLOMBLTdan9XU',
+        range,
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+          values: [[emailAddress]],
+        },
+      },
+      (err, response) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(response)
+        }
+      }
+    )
+  })
+}
+
+exports.handler = async function(event, context, callback) {
+  try {
+    const emailToSignup = JSON.parse(event.body).email
+    const sheetsRes = await addToCol('Local!F2', emailToSignup) // don't resolve though. Not going to fix if failure.
+    return {
+      statusCode: sheetsRes.status,
+      body: JSON.stringify(sheetsRes),
+    }
+  } catch (err) {
+    console.log(err)
+    return { statusCode: 500, body: err.toString() }
+  }
 }
