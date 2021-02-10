ba57f8e783ebe83cf4783f63a04f89dcc7b7c74f
YuliaBakhar <bakhar.yulia@gmail.com>
2021-02-10 20:00:24 +0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,22 @@ const pageStyles = {
 
 // markup
 const IndexPage = () => {
+  function handleClick(e) {
+    fetch('/.netlify/functions/newsletter-signed-up')
+      .then((response) => response.json())
+      .then(console.log);
+  }
+
   return (
     <main style={pageStyles}>
-      <p>
-        Stay tuned in as we discover more of this mysterious world. New products
-        and stories every week.
-      </p>
-      <input />
-      <button>sign me up</button>
+      <form onSubmit={handleClick}>
+        <p>
+          Stay tuned in as we discover more of this mysterious world. New
+          products and stories every week.
+        </p>
+        <input />
+        <button>sign me up</button>
+      </form>
     </main>
   );
 };
