17d56f8b83fac1b3e9c2e60121b4f3b2264a9653
YuliaBakhar <bakhar.yulia@gmail.com>
2021-02-11 20:42:04 +0300

    pew3

--- functions/newsletter-signed-up.js
+++ functions/newsletter-signed-up.js
@@ -26,26 +26,43 @@ exports.handler = async function (event, context, callback) {
 
     console.log(list, 'list 1');
     if (!list) {
-      try {
-        list = await fetch('https://api.sendinblue.com/v3/contacts/lists', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'api-key': SENDINBLUE_API_KEY,
-          },
-          body: JSON.stringify({ name: location, folderId: 7 }),
-        });
+      list = await fetch('https://api.sendinblue.com/v3/contacts/lists', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'api-key': SENDINBLUE_API_KEY,
+        },
+        body: JSON.stringify({ name: location, folderId: 7 }),
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.error('error:' + err));
 
-        console.log(list, 'list 2');
-      } catch (err) {
-        console.error('error:' + err);
-      }
+      console.log(list, 'list 2');
     }
 
     const listId = list.id;
 
     console.log(listId, 'listId');
+
+    const contact = await fetch('https://api.sendinblue.com/v3/contacts', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'api-key': SENDINBLUE_API_KEY,
+      },
+      body: JSON.stringify({
+        listIds: [{ listId }],
+        updateEnabled: false,
+        email: 'test@gmail.com',
+      }),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.error('error:' + err));
+
+    console.log(contact, 'contact');
   } catch (err) {
     console.log(err);
     return { statusCode: 500, body: err.toString() };
