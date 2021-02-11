2447b801a12c48f94958cf96ec89d856576b34ad
YuliaBakhar <bakhar.yulia@gmail.com>
2021-02-11 16:04:34 +0300

    peew

--- functions/newsletter-signed-up.js
+++ functions/newsletter-signed-up.js
@@ -2,6 +2,18 @@ exports.handler = async function (event, context, callback) {
   try {
     console.log(event, 'event');
     console.log('click');
+
+    const lists = await fetch('https://api.sendinblue.com/v3/contacts/lists', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': SENDINBLUE_API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .catch((err) => console.error('error:' + err));
+
+    console.log(lists, 'list');
   } catch (err) {
     console.log(err);
     return { statusCode: 500, body: err.toString() };
